a5d2dd0e50e8be773ccef9ec939a1f123e92bb6b
Merijn <merijn@ponzo.nl>
2023-09-12 11:32:23 +0200

    fix

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ import { getAspectRatio } from './lib/aspect-ratio';
 import { getContainer } from './lib/container';
 import { getSpacing } from './lib/spacing';
 import { getScreens } from './lib/screens';
-import { getSafelist } from './lib/safelist';
 import { themeParser } from './lib/theme-parser';
 
 
 
 // Export the function so it's available for import in other files
-export { getAspectRatio, getContainer, getSpacing, getScreens, getSafelist, themeParser };
+export { getAspectRatio, getContainer, getSpacing, getScreens, themeParser };
