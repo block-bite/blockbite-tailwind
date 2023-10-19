0105e59b53286de708cc39d7be74f32bceec6945
Merijn <merijn@ponzo.nl>
2023-10-19 23:10:34 +0200

    fuixes

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ import { getSpacing } from './lib/spacing';
 import { gridSpacing } from './lib/spacing';
 import { clampSpacing } from './lib/spacing';
 import { getScreens } from './lib/screens';
+import { getScreenDimensions } from './lib/screen-dimensions';
 import { getColors, getColorObject} from './lib/colors';
 import { themeParser } from './lib/theme-parser';
 
 
 // Export the function so it's available for import in other files
-export { getAspectRatio, getContainer, getSpacing, getColors, getColorObject, gridSpacing, clampSpacing, getScreens, themeParser };
+export { getAspectRatio, getContainer, getSpacing, getColors, getColorObject, gridSpacing, clampSpacing, getScreens, getScreenDimensions, themeParser };
