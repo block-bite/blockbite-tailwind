66ebe2cab5a9af889f97ce2edaca49b375586155
Merijn <merijn@ponzo.nl>
2024-02-27 10:35:53 +0100

    wider container

--- components/fluid-container.js
+++ components/fluid-container.js
@@ -1,26 +1,32 @@
 module.exports = function ({ addComponents, theme }) {
-	const container = {
-	  '.container-fluid': {
-		maxWidth: '99vw',
-		marginLeft: 'auto',
-		marginRight: 'auto',
-		paddingLeft: '1rem',
-		paddingRight: '1rem',
-	  },
-	};
-  
-	// Access the '2xl' screen size from Tailwind CSS configuration
-	const maxWidth2xl = theme('screens.2xl');
-  
-	// Add a media query for screens at '2xl' size
-	if (maxWidth2xl) {
-	  container[`@media (min-width: ${maxWidth2xl})`] = {
-		'.container-fluid': {
-		  maxWidth: maxWidth2xl,
-		},
-	  };
-	}
-  
-	addComponents(container);
+  const container = {
+    ".container-fluid": {
+      maxWidth: "95vw",
+      marginLeft: "auto",
+      marginRight: "auto",
+      paddingLeft: "1rem",
+      paddingRight: "1rem",
+    },
   };
-  
+
+  const maxWidth2xl = theme("screens.2xl");
+  const maxWidthSm = theme("screens.xs");
+
+  // Add a media query for screens at '2xl' size
+  if (maxWidth2xl) {
+    container[`@media (min-width: ${maxWidth2xl})`] = {
+      ".container-fluid": {
+        maxWidth: maxWidth2xl,
+      },
+    };
+  }
+  if (maxWidthSm) {
+    container[`@media (max-width: ${maxWidthSm})`] = {
+      ".container-fluid": {
+        maxWidth: "98vw",
+      },
+    };
+  }
+
+  addComponents(container);
+};
