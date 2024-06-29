607f8d63f83cb81f0d9a2561be2460cdc0441849
Merijn <merijn@ponzo.nl>
2024-06-29 15:42:23 +0200

    fix custom screen

--- plugins/viewport-dimensions.js
+++ plugins/viewport-dimensions.js
@@ -4,27 +4,27 @@ module.exports = plugin(function ({ addUtilities }) {
   const utilities = {};
 
   for (let i = 10; i <= 100; i += 10) {
-    utilities[`.w-screen-${i}`] = {
+    utilities[`.b_w-screen-${i}`] = {
       width: `${i}vw`,
     };
 
-    utilities[`.h-screen-${i}`] = {
+    utilities[`.b_h-screen-${i}`] = {
       height: `${i}vh`,
     };
 
-    utilities[`.min-h-screen-${i}`] = {
+    utilities[`.b_min-h-screen-${i}`] = {
       minHeight: `${i}vh`,
     };
 
-    utilities[`.max-h-screen-${i}`] = {
+    utilities[`.b_max-h-screen-${i}`] = {
       maxHeight: `${i}vh`,
     };
 
-    utilities[`.min-w-screen-${i}`] = {
+    utilities[`.b_min-w-screen-${i}`] = {
       minWidth: `${i}vw`,
     };
 
-    utilities[`.max-w-screen-${i}`] = {
+    utilities[`.b_max-w-screen-${i}`] = {
       maxWidth: `${i}vw`,
     };
   }
