9a7a9ac8efe1cd16419c0ae2e4c291e49b2fc580
Merijn <merijn@ponzo.nl>
2024-07-02 09:54:11 +0200

    b_ani

--- motion/utilities/offset.js
+++ motion/utilities/offset.js
@@ -1,12 +1,11 @@
 module.exports = function ({ addUtilities, theme, e }, gridSpacing) {
-
-  const spacing = theme('spacing')
+  const spacing = theme("spacing");
   const newUtilities = {};
 
   Object.entries(spacing).map(([key, value]) => {
-    newUtilities[`.${e(`ani-x-${key}`)}`] = { '--an-ani-x': `${value}` };
-    newUtilities[`.${e(`ani-y-${key}`)}`] = { '--an-ani-y': `${value}` };
-  })
+    newUtilities[`.${e(`b_ani-x-${key}`)}`] = { "--an-ani-x": `${value}` };
+    newUtilities[`.${e(`b_ani-y-${key}`)}`] = { "--an-ani-y": `${value}` };
+  });
 
-  addUtilities(newUtilities, ['responsive'])
+  addUtilities(newUtilities, ["responsive"]);
 };
