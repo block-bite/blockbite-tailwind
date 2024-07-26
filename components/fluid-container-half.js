4d7c660ab928fb1df5c83fcf04c956785c57aaad
Merijn <merijn@ponzo.nl>
2024-07-26 13:14:39 +0200

    fix

--- components/fluid-container-half.js
+++ components/fluid-container-half.js
@@ -1,13 +1,13 @@
 module.exports = function ({ addComponents, theme }) {
   const container = {
-    ".b_container-fluid-1/2-start": {
+    ".b_container-fluid-half-r": {
       maxWidth: "var(--container-fluid-xl)",
       marginLeft: "auto",
       marginRight: "auto",
       paddingLeft: "1rem",
       paddingRight: "1rem",
     },
-    ".b_container-fluid-1/2-end": {
+    ".b_container-fluid-half-l": {
       maxWidth: "var(--container-fluid-xl)",
       marginLeft: "auto",
       marginRight: "auto",
@@ -21,11 +21,11 @@ module.exports = function ({ addComponents, theme }) {
 
   if (maxWidthXl) {
     container[`@media (min-width: ${maxWidthXl})`] = {
-      ".b_container-fluid-1/2-start": {
+      ".b_container-fluid-half-l": {
         maxWidth: `calc(${maxWidthXl} / 2)`,
         marginLeft: `calc((100vw - ${maxWidthXl} - 1rem) / 2)`,
       },
-      ".b_container-fluid-1/2-end": {
+      ".b_container-fluid-half-r": {
         maxWidth: `calc(${maxWidthXl} / 2)`,
         marginRight: `calc((100vw - ${maxWidthXl} - 1rem) / 2 )`,
       },
@@ -33,11 +33,11 @@ module.exports = function ({ addComponents, theme }) {
   }
   if (maxWidth2xl) {
     container[`@media (min-width: ${maxWidth2xl})`] = {
-      ".b_container-fluid-1/2-start": {
+      ".b_container-fluid-half-l": {
         maxWidth: `calc(${maxWidth2xl} / 2)`,
         marginLeft: `calc((100vw - ${maxWidth2xl} - 1rem) / 2)`,
       },
-      ".b_container-fluid-1/2-end": {
+      ".b_container-fluid-half-r": {
         maxWidth: `calc(${maxWidth2xl} / 2)`,
         marginRight: `calc((100vw - ${maxWidth2xl} - 1rem) / 2)`,
       },
