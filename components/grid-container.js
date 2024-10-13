d2bc52758e51be56a3eea0e9c67111a72c9ae96b
Merijn <merijn@ponzo.nl>
2024-10-13 19:40:44 +0200

    add-grid-container

---
+++ components/grid-container.js
@@ -0,0 +1,12 @@
+module.exports = function ({ addComponents, theme }) {
+  const container = {
+    ".b_grid-container": {
+      display: grid,
+      gridTemplateColumns:
+        "1fr repeat(12, minmax(var(--b_column), var(--b_column))) 1fr",
+      gap: "var(--b_gap)",
+    },
+  };
+
+  addComponents(container);
+};
