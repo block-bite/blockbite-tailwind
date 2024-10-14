40f03b70a452d30f5765b38ab580b247d48cef9a
Merijn <merijn@ponzo.nl>
2024-10-14 10:33:02 +0200

    2.5.4

--- components/grid-container.js
+++ components/grid-container.js
@@ -1,10 +1,25 @@
 module.exports = function ({ addComponents, theme }) {
   const container = {
-    ".b_grid-container": {
+    // Responsive for min-width: 1440px - results 1440px;
+    ".b_grid-area-container": {
       display: "grid",
-      gridTemplateColumns:
-        "1fr repeat(12, minmax(var(--b_column), var(--b_column))) 1fr",
-      gap: "var(--b_gap)",
+      gridTemplateColumns: "1fr repeat(12, minmax(120px, 120px)) 1fr",
+      height: "100vh",
+
+      // Responsive for max-width: 1440px - results 1280px;
+      [`@media (max-width: 1440px)`]: {
+        gridTemplateColumns: "1fr repeat(12, minmax(90px, 90px)) 1fr",
+      },
+
+      // Responsive for max-width: 1080px : results 960px;
+      [`@media (max-width: 1080px)`]: {
+        gridTemplateColumns: "1fr repeat(12, minmax(80px, 80px)) 1fr",
+      },
+
+      // Responsive for max-width: 960px : results auto
+      [`@media (max-width: 960px)`]: {
+        gridTemplateColumns: "0px repeat(12, 1fr) 0px",
+      },
     },
   };
 
