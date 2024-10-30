d26a2a5cc8db34120580130556a6de1d6de9ce83
Merijn <merijn@ponzo.nl>
2024-10-30 15:22:02 +0100

    remove height from container add important

--- components/grid-container.js
+++ components/grid-container.js
@@ -3,22 +3,21 @@ module.exports = function ({ addComponents, theme }) {
     // Responsive for min-width: 1440px - results 1440px;
     ".b_grid-container": {
       display: "grid",
-      gridTemplateColumns: "1fr repeat(12, minmax(120px, 120px)) 1fr",
-      height: "100vh",
-
+      gridTemplateColumns:
+        "1fr repeat(12, minmax(120px, 120px)) 1fr !important",
       // Responsive for max-width: 1440px - results 1280px;
       [`@media (max-width: 1440px)`]: {
-        gridTemplateColumns: "1fr repeat(12, minmax(90px, 90px)) 1fr",
+        gridTemplateColumns:
+          "1fr repeat(12, minmax(90px, 90px)) 1fr !important",
       },
-
       // Responsive for max-width: 1080px : results 960px;
       [`@media (max-width: 1080px)`]: {
-        gridTemplateColumns: "1fr repeat(12, minmax(80px, 80px)) 1fr",
+        gridTemplateColumns:
+          "1fr repeat(12, minmax(80px, 80px)) 1fr !important",
       },
-
       // Responsive for max-width: 960px : results auto
       [`@media (max-width: 960px)`]: {
-        gridTemplateColumns: "0px repeat(12, 1fr) 0px",
+        gridTemplateColumns: "0px repeat(12, 1fr) 0px !important",
       },
     },
   };
--- gridarea/utilities/grid-area.js
+++ gridarea/utilities/grid-area.js
@@ -6,22 +6,18 @@ module.exports = function ({ addComponents, theme }) {
           "var(--b_area-y) / var(--b_area-x) / calc(var(--b_area-y) + var(--b_area-h)) / calc(var(--b_area-x) + var(--b_area-w))",
       },
       ".b_grid-area-16": {
-        display: "grid",
         gridTemplateColumns: "repeat(16, 1fr)",
         gridTemplateRows: "repeat(16, 1fr)",
       },
       ".b_grid-area-32": {
-        display: "grid",
         gridTemplateColumns: "repeat(32, 1fr)",
         gridTemplateRows: "repeat(32, 1fr)",
       },
       ".b_grid-area-64": {
-        display: "grid",
         gridTemplateColumns: "repeat(64, 1fr)",
         gridTemplateRows: "repeat(64, 1fr)",
       },
       ".b_grid-area-96": {
-        display: "grid",
         gridTemplateColumns: "repeat(96, 1fr)",
         gridTemplateRows: "repeat(96, 1fr)",
       },
