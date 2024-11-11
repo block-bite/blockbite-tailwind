de8787e1d4731f6c32209c841a0bdee064eba0b0
Merijn <merijn@ponzo.nl>
2024-11-11 19:02:19 +0100

    2.6.4

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,17 @@
+// index.d.ts
 declare module "blockbite-tailwind" {
-  const content: any;
-  export default content;
+  export function getAspectRatio(...args: any[]): any;
+  export function getContainer(...args: any[]): any;
+  export function getGridClampSpacing(...args: any[]): any;
+  export function getScreens(...args: any[]): any;
+  export function getColors(...args: any[]): any;
+  export function getColorObject(...args: any[]): any;
+  export function themeParser(...args: any[]): any;
+
+  export const gridSpacing: any;
+  export const clampSpacing: any;
+  export const screenSpacing: any;
+  export const percentSpacing: any;
+  export const nativeSpacing: any;
+  export const spanSpacing: any;
 }
