e4e69b40cf77642a1cdb0ab1399ce101eb441f85
Merijn <merijn@ponzo.nl>
2024-11-21 20:02:50 +0100

    2.6.7

--- components/interaction.js
+++ components/interaction.js
@@ -1,23 +1,23 @@
 module.exports = function ({ addComponents }) {
   const b_tileslide_in = {
-    ".b_tileslide_in": {
+    ".b_tile-in-left": {
       position: "absolute",
       top: 0,
-      left: "-100%",
+      left: 0,
       right: 0,
       bottom: 0,
       zIndex: 10,
       overflow: "hidden",
       transition: "transform 0.5s",
-      transform: "translateX(0)", // Default position
+      transform: "translateX(-100%)",
     },
-    ".b_active .b_tileslide_in": {
-      transform: "translateX(100%)",
+    ".b_active .b_tile-in-left": {
+      transform: "translateX(0%)",
     },
   };
 
   const b_tilefade_in = {
-    ".b_tilefade_in": {
+    ".b_tile-in-fade": {
       opacity: 0,
       transition: "opacity 0.5s",
       position: "absolute",
@@ -28,7 +28,7 @@ module.exports = function ({ addComponents }) {
       zIndex: 10,
       overflow: "hidden",
     },
-    ".b_active .b_tilefade_in": {
+    ".b_active .b_tile-in-fade": {
       opacity: 1,
     },
   };
