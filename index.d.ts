35c4a43aa2ad4448b34fc356172e4af9f0ea96a9
Merijn <merijn@ponzo.nl>
2024-12-06 10:36:44 +0100

    enhanced exports

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,17 @@
-// index.d.ts
 declare module "blockbite-tailwind" {
+  // Function declarations
   export function getAspectRatio(...args: any[]): any;
   export function getContainer(...args: any[]): any;
-  export function getGridClampSpacing(...args: any[]): any;
   export function getScreens(...args: any[]): any;
   export function getColors(...args: any[]): any;
   export function getColorObject(...args: any[]): any;
   export function themeParser(...args: any[]): any;
 
-  export const gridSpacing: any;
-  export const clampSpacing: any;
-  export const screenSpacing: any;
-  export const percentSpacing: any;
-  export const nativeSpacing: any;
-  export const spanSpacing: any;
+// Constant declarations
+export const gridSpacing: any;
+export const fluidSpacing: any;
+export const screenSpacing: any;
+export const percentSpacing: any;
+export const nativeSpacing: any;
+export const spanSpacing: any;
 }
--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-// Import the function from the appropriate location
+// Import functions and constants
 import { getAspectRatio } from "./lib/aspect-ratio";
 import { getContainer } from "./lib/container";
-import { gridSpacing } from "./lib/spacing";
-import { fluidSpacing } from "./lib/spacing";
-import { screenSpacing } from "./lib/spacing";
-import { percentSpacing } from "./lib/spacing";
-import { nativeSpacing } from "./lib/spacing";
-import { spanSpacing } from "./lib/spacing";
+import {
+  gridSpacing,
+  fluidSpacing,
+  screenSpacing,
+  percentSpacing,
+  nativeSpacing,
+  spanSpacing,
+} from "./lib/spacing";
 import { getScreens } from "./lib/screens";
 import { getColors, getColorObject } from "./lib/colors";
 import { themeParser } from "./lib/theme-parser";
 
-// Export the function so it's available for import in other files
+// Export functions and constants
 export {
   getAspectRatio,
   getContainer,
-  getColors,
-  getColorObject,
   gridSpacing,
-  nativeSpacing,
   fluidSpacing,
   screenSpacing,
-  spanSpacing,
   percentSpacing,
+  nativeSpacing,
+  spanSpacing,
   getScreens,
+  getColors,
+  getColorObject,
   themeParser,
 };
