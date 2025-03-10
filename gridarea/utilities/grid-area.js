da8062ac343efe68588b1cf2991988935d4af652
Merijn <merijn@ponzo.nl>
2025-03-10 14:46:58 +0100

    3.1.6

--- gridarea/utilities/grid-area.js
+++ gridarea/utilities/grid-area.js
@@ -9,23 +9,22 @@ module.exports = function ({ addComponents, matchUtilities, theme, config }) {
       gridArea:
         "var(--b_area-y) / var(--b_area-x) / calc(var(--b_area-y) + var(--b_area-h)) / calc(var(--b_area-x) + var(--b_area-w))",
     },
-
-    ".b_grid-area-16": {
+    [`${prefix} .b_grid-area-16`]: {
       display: "grid",
       gridTemplateColumns: "repeat(16, 1fr)",
       gridTemplateRows: "repeat(16, 1fr)",
     },
-    ".b_grid-area-32": {
+    [`${prefix} .b_grid-area-32`]: {
       display: "grid",
       gridTemplateColumns: "repeat(32, 1fr)",
       gridTemplateRows: "repeat(32, 1fr)",
     },
-    ".b_grid-area-64": {
+    [`${prefix} .b_grid-area-64`]: {
       display: "grid",
       gridTemplateColumns: "repeat(64, 1fr)",
       gridTemplateRows: "repeat(64, 1fr)",
     },
-    ".b_grid-area-96": {
+    [`${prefix} .b_grid-area-96`]: {
       display: "grid",
       gridTemplateColumns: "repeat(96, 1fr)",
       gridTemplateRows: "repeat(96, 1fr)",
